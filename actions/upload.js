3bfca5139731afca5fd2f936124f95262513e4eb
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-18 12:21:37 +1000

    Refactor to use collections/list compatible functions

--- actions/upload.js
+++ actions/upload.js
@@ -1,10 +1,13 @@
+var debug = require('debug')('dilemma:action-upload');
+
 module.exports = function(socket, db) {
   return function(source, result) {
     // find the specified challenger
     var challenger = db.active.filter(function(chal) {
       return chal.source === source;
-    })[0];
+    }).one();
 
+    debug('running upload action for challenger: ', challenger);
     if (challenger) {
       challenger.addResult(result);
     }
