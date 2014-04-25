855158562e18ccba782320a9ddf5b8bd1e4dedb9
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-25 18:04:15 +1000

    Remove old code

--- matchup.js
+++ matchup.js
@@ -131,35 +131,3 @@ module.exports = pull.Sink(function(read, server, db, done) {
 
   read(null, next);
 });
-
-// module.exports = function(iterations, a, b) {
-//   function handleResult() {
-//     var counts = [a, b].map(pluck('results')).map(length);
-//     if (counts[0] === counts[1]) {
-//       if (counts[0] < iterations) {
-//         iterate();
-//       }
-//       else {
-//         finish();
-//       }
-//     }
-//   }
-
-//   function iterate() {
-//     [a, b].forEach(function(challenger) {
-//       challenger.run();
-//     });
-//   }
-
-//   function finish() {
-//     debug('finished');
-//     [a, b].map(function(challenger) {
-//       challenger.send('end');
-//     });
-//   }
-
-//   debug('running matchup: ', a.name, b.name);
-//   [a, b].forEach(function(challenger) {
-//     challenger.on('result', handleResult).run();
-//   });
-// };
