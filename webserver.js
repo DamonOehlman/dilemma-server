24afcfb833299cf83776417e0466e447230b7a06
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-25 14:40:24 +1000

    Progress on data streams

--- webserver.js
+++ webserver.js
@@ -6,6 +6,9 @@ var debug = require('debug')('dilemma:webserver');
 var fs = require('fs');
 var http = require('http');
 var mapleTree = require('mapleTree');
+var pl = require('pull-level');
+var pull = require('pull-stream');
+var sse = require('pull-sse');
 var port = parseInt(process.env.NODE_PORT || config.ports.http, 10);
 var st = require('st');
 var browserify = require('browserify');
@@ -22,26 +25,59 @@ module.exports = function(socket, db) {
   var router = new mapleTree.RouteTree();
   var server = http.createServer(handleRequest);
 
+  function browserifyJS(req, res) {
+    var targetFile = __dirname + '/static/_js/' + this.params.file + '.js';
+    var b;
+
+    debug('attempting to browserify file: ' + targetFile);
+    if (! fs.existsSync(targetFile)) {
+      return notfound(req, res);
+    }
+
+    res.writeHead(200, {
+      'content-type': 'text/javascript'
+    });
+
+    return browserify(targetFile).bundle().pipe(res);
+  }
+
   function handleRequest(req, res) {
     mount(req, res, function() {
-      var targetFile = __dirname + '/static' + req.url;
-      var b;
+      var match = router.match(req.url);
 
-      if (reJS.test(req.url) && fs.existsSync(targetFile)) {
-        debug('browserifying file: ' + req.url)
-        res.writeHead(200, {
-          'content-type': 'text/javascript'
-        });
-
-        return browserify(__dirname + '/static' + req.url).bundle().pipe(res);
+      debug('url requested: ' + req.url, match && typeof match.fn);
+      if (match && typeof match.fn == 'function') {
+        return match.fn(req, res);
       }
 
-      debug('no static file for: ' + req.url);
-
-      res.writeHead(404);
-      res.end('Not found');
+      return notfound(req, res);
     });
   }
+
+  function notfound(req, res) {
+    debug('no static file for: ' + req.url);
+
+    res.writeHead(404);
+    res.end('Not found');
+  }
+
+  function serveData(req, res) {
+    var endpoint = db[this.params.endpoint];
+
+    // if we don't have an endpoint, then return notfound
+    debug('looking for data endpoint: ' + this.params.endpoint + ', found: ' + (!!endpoint));
+    if (! endpoint) {
+      return notfound(req, res);
+    }
+
+    return pull(
+      pl.read(endpoint, { tail: true }),
+      sse(res)
+    );
+  }
+
+  router.define('/js/:file.js', browserifyJS);
+  router.define('/data/:endpoint', serveData);
 
   // start the server
   server.listen(port, function() {
