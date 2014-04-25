2b23a625a0f0125d48232129571008777d4cb48d
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-25 13:40:21 +1000

    Add port configuration

--- config.js
+++ config.js
@@ -1,5 +1,13 @@
+// initialise the default port the webserver will listen on
+exports.ports = {
+  http: 1442,
+  zmq: 1441
+};
+
+// initialise the number of iterations that should be completed for a matchup
 exports.iterations = 1000;
 
+// initialise the years that will be served by each party in cases
 exports.scores = {
   'DD': [ 3, 3 ], // both betray
   'CD': [ 5, 0 ], // a remain silent, b betray
